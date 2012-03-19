196a9d12b6c760e9c9a56f17c836223370c408a4
Osman KOCAK <kocakosm@gmail.com>
2012-03-19 22:37:57 +0100

    Added MD2, MD5, SHA1, SHA256 and SHA512 digests (based on Java's MessageDigest)

--- src/org/pitaya/digest/Digests.java
+++ src/org/pitaya/digest/Digests.java
@@ -16,6 +16,9 @@
 
 package org.pitaya.digest;
 
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
 /**
  * Somme commonly used digest algorithms. Careful: some of these algorithms are
  * not suitable for security related applications.
@@ -24,34 +27,148 @@ package org.pitaya.digest;
  */
 public final class Digests
 {
+	/**
+	 * Returns a new MD2 {@link Digest} instance.
+	 *
+	 * @return a new MD2 {@link Digest} instance.
+	 */
 	public static Digest md2()
 	{
-		throw new UnsupportedOperationException();
+		return new CustomDigest("MD2");
 	}
 
+	/**
+	 * Returns a new MD4 {@link Digest} instance.
+	 *
+	 * @return a new MD4 {@link Digest} instance.
+	 */
 	public static Digest md4()
 	{
 		return new MD4();
 	}
 
+	/**
+	 * Returns a new MD5 {@link Digest} instance.
+	 *
+	 * @return a new MD5 {@link Digest} instance.
+	 */
 	public static Digest md5()
 	{
-		throw new UnsupportedOperationException();
+		return new CustomDigest("MD5");
 	}
 
+	/**
+	 * Returns a new SHA1 {@link Digest} instance.
+	 *
+	 * @return a new SHA1 {@link Digest} instance.
+	 */
 	public static Digest sha1()
 	{
-		throw new UnsupportedOperationException();
+		return new CustomDigest("SHA1");
 	}
 
+	/**
+	 * Returns a new SHA-256 {@link Digest} instance.
+	 *
+	 * @return a new SHA-256 {@link Digest} instance.
+	 */
 	public static Digest sha256()
 	{
-		throw new UnsupportedOperationException();
+		return new CustomDigest("SHA-256");
 	}
 
+	/**
+	 * Returns a new SHA-512 {@link Digest} instance.
+	 *
+	 * @return a new SHA-512 {@link Digest} instance.
+	 */
 	public static Digest sha512()
 	{
-		throw new UnsupportedOperationException();
+		return new CustomDigest("SHA-512");
+	}
+
+	private static final class CustomDigest implements Digest
+	{
+		private final String algorithm;
+		private final MessageDigest md;
+
+		CustomDigest(String algorithm)
+		{
+			this.algorithm = algorithm;
+			try {
+				this.md = MessageDigest.getInstance(algorithm);
+			} catch (NoSuchAlgorithmException ex) {
+				/* Cannot happen... */
+				throw new IllegalStateException();
+			}
+		}
+
+		@Override
+		public int length()
+		{
+			return md.getDigestLength();
+		}
+
+		@Override
+		public void reset()
+		{
+			md.reset();
+		}
+
+		@Override
+		public void update(byte input)
+		{
+			md.update(input);
+		}
+
+		@Override
+		public void update(byte... input)
+		{
+			md.update(input);
+		}
+
+		@Override
+		public void update(byte[] input, int off, int len)
+		{
+			md.update(input, off, len);
+		}
+
+		@Override
+		public byte[] digest(byte... input)
+		{
+			return md.digest(input);
+		}
+
+		@Override
+		public byte[] digest()
+		{
+			return md.digest();
+		}
+
+		@Override
+		public String toString()
+		{
+			return algorithm;
+		}
+
+		@Override
+		public boolean equals(Object o)
+		{
+			if (o == this) {
+				return true;
+			}
+			if (!(o instanceof CustomDigest)) {
+				return false;
+			}
+			final CustomDigest digest = (CustomDigest) o;
+			return md.equals(digest.md);
+		}
+
+		@Override
+		public int hashCode()
+		{
+			return md.hashCode();
+		}
 	}
 
 	private Digests()
---
+++ test/org/pitaya/digest/DigestsTest.java
@@ -0,0 +1,141 @@
+/*----------------------------------------------------------------------------*
+ * This file is part of Pitaya.                                               *
+ * Copyright (C) 2012 Osman KOCAK <kocakosm@gmail.com>                        *
+ *                                                                            *
+ * This program is free software: you can redistribute it and/or modify it    *
+ * under the terms of the GNU Lesser General Public License as published by   *
+ * the Free Software Foundation, either version 3 of the License, or (at your *
+ * option) any later version.                                                 *
+ * This program is distributed in the hope that it will be useful, but        *
+ * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY *
+ * or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public     *
+ * License for more details.                                                  *
+ * You should have received a copy of the GNU Lesser General Public License   *
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.       *
+ *----------------------------------------------------------------------------*/
+
+package org.pitaya.digest;
+
+import static org.junit.Assert.*;
+
+import org.pitaya.charset.ASCII;
+import org.pitaya.util.Base16;
+
+import org.junit.Test;
+
+/**
+ * Digests' unit tests.
+ *
+ * @author Osman KOCAK
+ */
+public final class DigestsTest
+{
+	private static final String PANGRAM;
+	private static final String EMPTY_STRING;
+	static {
+		PANGRAM = "The quick brown fox jumps over the lazy dog";
+		EMPTY_STRING = "";
+	}
+
+	@Test
+	public void testMD2()
+	{
+		Digest md2 = Digests.md2();
+		assertThat(EMPTY_STRING).hashedWith(md2)
+			.isEqualTo("8350e5a3e24c153df2275c9f80692773");
+		assertThat(PANGRAM).hashedWith(md2)
+			.isEqualTo("03d85a0d629d2c442e987525319fc471");
+	}
+
+	@Test
+	public void testMD4()
+	{
+		Digest md4 = Digests.md4();
+		assertThat(EMPTY_STRING).hashedWith(md4)
+			.isEqualTo("31d6cfe0d16ae931b73c59d7e0c089c0");
+		assertThat(PANGRAM).hashedWith(md4)
+			.isEqualTo("1bee69a46ba811185c194762abaeae90");
+	}
+
+	@Test
+	public void testMD5()
+	{
+		Digest md5 = Digests.md5();
+		assertThat(EMPTY_STRING).hashedWith(md5)
+			.isEqualTo("d41d8cd98f00b204e9800998ecf8427e");
+		assertThat(PANGRAM).hashedWith(md5)
+			.isEqualTo("9e107d9d372bb6826bd81d3542a419d6");
+
+	}
+
+	@Test
+	public void testSHA1()
+	{
+		Digest sha1 = Digests.sha1();
+		assertThat(EMPTY_STRING).hashedWith(sha1)
+			.isEqualTo("da39a3ee5e6b4b0d3255bfef95601890afd80709");
+		assertThat(PANGRAM).hashedWith(sha1)
+			.isEqualTo("2fd4e1c67a2d28fced849ee1bb76e7391b93eb12");
+	}
+
+	@Test
+	public void testSHA256()
+	{
+		Digest sha256 = Digests.sha256();
+		assertThat(EMPTY_STRING).hashedWith(sha256)
+			.isEqualTo("e3b0c44298fc1c149afbf4c8996fb92427ae41e4649"
+				+ "b934ca495991b7852b855");
+		assertThat(PANGRAM).hashedWith(sha256)
+			.isEqualTo("d7a8fbb307d7809469ca9abcb0082e4f8d5651e46d3"
+				+ "cdb762d02d0bf37c9e592");
+	}
+
+	@Test
+	public void testSHA512()
+	{
+		Digest sha512 = Digests.sha512();
+		assertThat(EMPTY_STRING).hashedWith(sha512)
+			.isEqualTo("cf83e1357eefb8bdf1542850d66d8007d620e4050b5"
+				+ "715dc83f4a921d36ce9ce47d0d13c5d85f2b0ff8318d"
+				+ "2877eec2f63b931bd47417a81a538327af927da3e");
+		assertThat(PANGRAM).hashedWith(sha512)
+			.isEqualTo("07e547d9586f6a73f73fbac0435ed76951218fb7d0c"
+				+ "8d788a309d785436bbb642e93a252a954f23912547d1"
+				+ "e8a3b5ed6e1bfd7097821233fa0538f3db854fee6");
+	}
+
+	private static Input assertThat(String input)
+	{
+		return new Input(input);
+	}
+
+	private static final class Input
+	{
+		private final byte[] data;
+
+		Input(String input)
+		{
+			this.data = ASCII.encode(input);
+		}
+
+		Result hashedWith(Digest digest)
+		{
+			return new Result(digest.digest(data));
+		}
+	}
+
+	private static final class Result
+	{
+		private final byte[] digest;
+
+		Result(byte[] digest)
+		{
+			this.digest = digest;
+		}
+
+		void isEqualTo(String expected)
+		{
+			assertArrayEquals(Base16.decode(expected), digest);
+		}
+	}
+}
