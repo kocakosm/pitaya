62474878affcdfdb4e39fabc29ca79cddb739547
Osman KOCAK <kocakosm@gmail.com>
2012-03-28 01:01:35 +0200

    Unit tests for the Passwords utility class

---
+++ test/org/pitaya/util/PasswordsTest.java
@@ -0,0 +1,56 @@
+/*----------------------------------------------------------------------------*
+ * This file is part of Pitaya.                                               *
+ * Copyright (C) 2012 Osman KOCAK <kocakosm@gmail.com>                        *
+ *                                                                            *
+ * This program is free software: you can redistribute it and/or modify it    *
+ * under the terms of the GNU Lesser General Public License as published by   *
+ * the Free Software Foundation, either version 3 of the License, or (at your *
+ * option) any later version.                                                 *
+ * This program is distributed in the hope that it will be useful, but        *
+ * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY *
+ * or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public     *
+ * License for more details.                                                  *
+ * You should have received a copy of the GNU Lesser General Public License   *
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.       *
+ *----------------------------------------------------------------------------*/
+
+package org.pitaya.util;
+
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+import java.util.Arrays;
+
+import org.junit.Test;
+
+/**
+ * {@link Passwords}' unit tests.
+ *
+ * @author Osman KOCAK
+ */
+public final class PasswordsTest
+{
+	@Test
+	public void testHashAndVerify()
+	{
+		String password = "password";
+		byte[] hash = Passwords.hash(password);
+		assertTrue(Passwords.verify(password, hash));
+	}
+
+	@Test
+	public void testInvalidPassword()
+	{
+		byte[] hash = Passwords.hash("password");
+		assertFalse(Passwords.verify("Password", hash));
+	}
+
+	@Test
+	public void testHashSalted()
+	{
+		String password = "password";
+		byte[] hash1 = Passwords.hash(password);
+		byte[] hash2 = Passwords.hash(password);
+		assertFalse(Arrays.equals(hash1, hash2));
+	}
+}
