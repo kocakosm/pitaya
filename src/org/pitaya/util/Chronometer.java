e4da3f4a0caaae5129d96f3fef31586f44b27725
Osman KOCAK <kocakosm@gmail.com>
2012-04-23 00:48:02 +0200

    Added a Chronometer class

---
+++ src/org/pitaya/util/Chronometer.java
@@ -0,0 +1,126 @@
+/*----------------------------------------------------------------------------*
+ * This file is part of Pitaya.                                               *
+ * Copyright (C) 2012 Osman KOCAK <kocakosm@gmail.com>                        *
+ *                                                                            *
+ * This program is free software: you can redistribute it and/or modify it    *
+ * under the terms of the GNU Lesser General Public License as published by   *
+ * the Free Software Foundation, either version 3 of the License, or (at your *
+ * option) any later version.                                                 *
+ * This program is distributed in the hope that it will be useful, but        *
+ * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY *
+ * or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public     *
+ * License for more details.                                                  *
+ * You should have received a copy of the GNU Lesser General Public License   *
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.       *
+ *----------------------------------------------------------------------------*/
+
+package org.pitaya.util;
+
+/**
+ * Utility class to measure elapsed time between specific instants. Precision is
+ * the millisecond (modulo the overhead of measurement). This class heavily
+ * relies on {@link System#currentTimeMillis()}. Instances of this class are not
+ * thread-safe.
+ *
+ * @author	Osman KOCAK
+ */
+public final class Chronometer
+{
+	private long referenceTime;
+	private long elapsedTime;
+	private boolean isRunning;
+
+	/** Creates a new (unstarted) {@code Chronometer}. */
+	public Chronometer()
+	{
+		this.referenceTime = now();
+		this.elapsedTime = 0;
+		this.isRunning = false;
+	}
+
+	/**
+	 * Returns {@code true} if {@link #start()} has been called on this
+	 * object, and {@link #stop()} hasn't been called since the last call
+	 * to {@code start()}.
+	 *
+	 * @return whether this chronometer is running.
+	 */
+	public boolean isRunning()
+	{
+		return isRunning;
+	}
+
+	/**
+	 * Starts this chronometer.
+	 * 
+	 * @return this object.
+	 *
+	 * @throws IllegalStateException if this chronometer is already running.
+	 */
+	public Chronometer start()
+	{
+		if (isRunning) {
+			throw new IllegalStateException("Already running...");
+		}
+		referenceTime = now();
+		isRunning = true;
+		return this;
+	}
+
+	/**
+	 * Stops this chronometer.
+	 *
+	 * @return this object.
+	 *
+	 * @throws IllegalStateException if this chronometer is already stopped.
+	 */
+	public Chronometer stop()
+	{
+		if (!isRunning) {
+			throw new IllegalStateException("Already idle...");
+		}
+		long now = now();
+		elapsedTime += now - referenceTime;
+		referenceTime = now;
+		isRunning = false;
+		return this;
+	}
+
+	/**
+	 * Resets this chronometer (sets the elapsed time for this chronometer
+	 * to zero). Calling this method does NOT modify this chronometer's
+	 * state (remains running if it was running, and, idle if it was idle).
+	 *
+	 * @return this object.
+	 */
+	public Chronometer reset()
+	{
+		this.elapsedTime = 0;
+		this.referenceTime = now();
+		return this;
+	}
+
+	/**
+	 * Returns the current elapsed time, expressed in milliseconds
+	 * 
+	 * @return the current elapsed time.
+	 */
+	public long elapsedTime()
+	{
+		if (isRunning) {
+			return elapsedTime + now() - referenceTime;
+		}
+		return elapsedTime;
+	}
+
+	@Override
+	public String toString()
+	{
+		return elapsedTime + " ms";
+	}
+
+	private long now()
+	{
+		return System.currentTimeMillis();
+	}
+}
---
+++ test/org/pitaya/util/ChronometerTest.java
@@ -0,0 +1,78 @@
+/*----------------------------------------------------------------------------*
+ * This file is part of Pitaya.                                               *
+ * Copyright (C) 2012 Osman KOCAK <kocakosm@gmail.com>                        *
+ *                                                                            *
+ * This program is free software: you can redistribute it and/or modify it    *
+ * under the terms of the GNU Lesser General Public License as published by   *
+ * the Free Software Foundation, either version 3 of the License, or (at your *
+ * option) any later version.                                                 *
+ * This program is distributed in the hope that it will be useful, but        *
+ * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY *
+ * or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public     *
+ * License for more details.                                                  *
+ * You should have received a copy of the GNU Lesser General Public License   *
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.       *
+ *----------------------------------------------------------------------------*/
+
+package org.pitaya.util;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+/**
+ * {@link Chronometer}'s unit tests.
+ *
+ * @author	Osman KOCAK
+ */
+public final class ChronometerTest
+{
+	@Test(expected=IllegalStateException.class)
+	public void testStopAlreadyIdle()
+	{
+		Chronometer sw = new Chronometer();
+		assertFalse(sw.isRunning());
+		sw.stop();
+	}
+
+	@Test(expected=IllegalStateException.class)
+	public void testStartAlreadyRunning()
+	{
+		Chronometer sw = new Chronometer().start();
+		assertTrue(sw.isRunning());
+		sw.start();
+	}
+
+	@Test
+	public void testElapsedTime() throws Exception
+	{
+		Chronometer chronometer = new Chronometer();
+		assertEquals(0, chronometer.elapsedTime());
+		chronometer.start();
+		Thread.sleep(100);
+		chronometer.stop();
+		assertTrue(Math.abs(chronometer.elapsedTime() - 100) < 10);
+		Thread.sleep(50);
+		chronometer.start();
+		Thread.sleep(100);
+		assertTrue(Math.abs(chronometer.elapsedTime() - 200) < 10);
+	}
+
+	@Test
+	public void testReset() throws Exception
+	{
+		Chronometer chronometer = new Chronometer();
+		chronometer.start();
+		Thread.sleep(50);
+		chronometer.stop();
+		chronometer.reset();
+		assertEquals(0, chronometer.elapsedTime());
+
+		chronometer.start();
+		Thread.sleep(50);
+		chronometer.reset();
+		Thread.sleep(100);
+		chronometer.stop();
+		assertTrue(Math.abs(chronometer.elapsedTime() - 100) < 10);
+	}
+}
