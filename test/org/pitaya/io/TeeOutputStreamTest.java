6b78cf3d13703835ee76e069d7df35565994503d
Osman KOCAK <kocakosm@gmail.com>
2013-04-14 16:10:29 +0200

    Added TeeOutputStream

---
+++ test/org/pitaya/io/TeeOutputStreamTest.java
@@ -0,0 +1,76 @@
+/*----------------------------------------------------------------------------*
+ * This file is part of Pitaya.                                               *
+ * Copyright (C) 2012 Osman KOCAK <kocakosm@gmail.com>                        *
+ *                                                                            *
+ * This program is free software: you can redistribute it and/or modify it    *
+ * under the terms of the GNU Lesser General Public License as published by   *
+ * the Free Software Foundation, either version 3 of the License, or (at your *
+ * option) any later version.                                                 *
+ * This program is distributed in the hope that it will be useful, but        *
+ * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY *
+ * or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public     *
+ * License for more details.                                                  *
+ * You should have received a copy of the GNU Lesser General Public License   *
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.       *
+ *----------------------------------------------------------------------------*/
+
+package org.pitaya.io;
+
+import static org.junit.Assert.*;
+
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+
+import org.junit.Test;
+
+/**
+ * {@link TeeOutputStream}'s unit tests.
+ *
+ * @author Osman KOCAK
+ */
+public final class TeeOutputStreamTest
+{
+	private static final byte[] DATA = {
+		(byte) 0x00, (byte) 0x11, (byte) 0x22, (byte) 0x33, (byte) 0x44,
+		(byte) 0x55, (byte) 0x66, (byte) 0x77, (byte) 0x88, (byte) 0x99,
+		(byte) 0xAA, (byte) 0xBB, (byte) 0xCC, (byte) 0xDD, (byte) 0xEE,
+	};
+
+	@Test
+	public void testWrite() throws IOException
+	{
+		ByteArrayOutputStream out1 = new ByteArrayOutputStream();
+		ByteArrayOutputStream out2 = new ByteArrayOutputStream();
+		TeeOutputStream tee = new TeeOutputStream(out1, out2);
+		for (byte b : DATA) {
+			tee.write(b);
+		}
+		tee.flush();
+		assertArrayEquals(DATA, out1.toByteArray());
+		assertArrayEquals(DATA, out2.toByteArray());
+		tee.close();
+	}
+
+	@Test
+	public void testWriteArray() throws IOException
+	{
+		ByteArrayOutputStream out1 = new ByteArrayOutputStream();
+		ByteArrayOutputStream out2 = new ByteArrayOutputStream();
+		TeeOutputStream tee = new TeeOutputStream(out1, out2);
+		tee.write(DATA);
+		tee.flush();
+		assertArrayEquals(DATA, out1.toByteArray());
+		assertArrayEquals(DATA, out2.toByteArray());
+		tee.close();
+
+		out1 = new ByteArrayOutputStream();
+		out2 = new ByteArrayOutputStream();
+		tee = new TeeOutputStream(out1, out2);
+		tee.write(DATA, 0, 8);
+		tee.write(DATA, 8, 7);
+		tee.flush();
+		assertArrayEquals(DATA, out1.toByteArray());
+		assertArrayEquals(DATA, out2.toByteArray());
+		tee.close();
+	}
+}
