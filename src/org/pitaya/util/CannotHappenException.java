2005deb74a4c3c0bbfbcd5ca2381f67256149059
Osman KOCAK <kocakosm@gmail.com>
2013-04-14 15:28:20 +0200

    Added CannotHappenException

---
+++ src/org/pitaya/util/CannotHappenException.java
@@ -0,0 +1,65 @@
+/*----------------------------------------------------------------------------*
+ * This file is part of Pitaya.                                               *
+ * Copyright (C) 2012 Osman KOCAK <kocakosm@gmail.com>                        *
+ *                                                                            *
+ * This program is free software: you can redistribute it and/or modify it    *
+ * under the terms of the GNU Lesser General Public License as published by   *
+ * the Free Software Foundation, either version 3 of the License, or (at your *
+ * option) any later version.                                                 *
+ * This program is distributed in the hope that it will be useful, but        *
+ * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY *
+ * or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public     *
+ * License for more details.                                                  *
+ * You should have received a copy of the GNU Lesser General Public License   *
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.       *
+ *----------------------------------------------------------------------------*/
+
+package org.pitaya.util;
+
+/**
+ * This exception is meant to be thrown in situation that cannot happen in 
+ * practice. Use with with parcimony.
+ *
+ * @author Osman KOCAK
+ */
+public final class CannotHappenException extends RuntimeException
+{
+	private static final long serialVersionUID = 3651678489121314654L;
+
+	/** Creates a new {@code CannotHappenException}. */
+	public CannotHappenException()
+	{
+		/* ... */
+	}
+
+	/**
+	 * Creates a new {@code CannotHappenException}.
+	 * 
+	 * @param message the error message.
+	 */
+	public CannotHappenException(String message)
+	{
+		super(message);
+	}
+
+	/**
+	 * Creates a new {@code CannotHappenException}.
+	 * 
+	 * @param cause the error that caused this one.
+	 */
+	public CannotHappenException(Throwable cause)
+	{
+		super(cause);
+	}
+
+	/**
+	 * Creates a new {@code CannotHappenException}.
+	 * 
+	 * @param message the error message.
+	 * @param cause the error that caused this one.
+	 */
+	public CannotHappenException(String message, Throwable cause)
+	{
+		super(message, cause);
+	}
+}
