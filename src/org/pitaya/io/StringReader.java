0fe4382ba24ea847f5ed11fa0dd9696d977b403a
Osman KOCAK <kocakosm@gmail.com>
2013-04-14 15:29:52 +0200

    Added StringReader

---
+++ src/org/pitaya/io/StringReader.java
@@ -0,0 +1,134 @@
+/*----------------------------------------------------------------------------*
+ * This file is part of Pitaya.                                               *
+ * Copyright (C) 2012 Osman KOCAK <kocakosm@gmail.com>                        *
+ *                                                                            *
+ * This program is free software: you can redistribute it and/or modify it    *
+ * under the terms of the GNU Lesser General Public License as published by   *
+ * the Free Software Foundation, either version 3 of the License, or (at your *
+ * option) any later version.                                                 *
+ * This program is distributed in the hope that it will be useful, but        *
+ * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY *
+ * or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public     *
+ * License for more details.                                                  *
+ * You should have received a copy of the GNU Lesser General Public License   *
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.       *
+ *----------------------------------------------------------------------------*/
+
+package org.pitaya.io;
+
+import org.pitaya.util.Parameters;
+
+import java.io.Reader;
+
+/**
+ * A {@link Reader} whose source are {@link String}s. Instances of this class
+ * behave exactly as {@link java.io.StringReader} instances except that they
+ * never throw {@link java.io.IOException}s. Not thread-safe.
+ *
+ * @author Osman KOCAK
+ */
+public final class StringReader extends Reader
+{
+	private final StringBuilder in;
+
+	private int index;
+	private int mark;
+
+	/**
+	 * Creates a new {@code StringReader}.
+	 *
+	 * @param strings the input {@link String}s.
+	 *
+	 * @throws NullPointerException if {@code strings} is {@code null} or if
+	 *	it contains a {@code null} reference.
+	 */
+	public StringReader(String... strings)
+	{
+		this.in = new StringBuilder(strings.length * 10);
+		for (String str : strings) {
+			Parameters.checkNotNull(str);
+			this.in.append(str);
+		}
+		this.in.trimToSize();
+	}
+
+	@Override
+	public boolean ready()
+	{
+		return true;
+	}
+
+	@Override
+	public int read()
+	{
+		return finished() ? -1 : in.charAt(index++);
+	}
+
+	@Override
+	public int read(char[] buf)
+	{
+		return read(buf, 0, buf.length);
+	}
+
+	@Override
+	public int read(char[] buf, int off, int len)
+	{
+		if (off < 0 || len < 0 || off + len > buf.length) {
+			throw new IndexOutOfBoundsException();
+		}
+		if (finished()) {
+			return -1;
+		}
+		int n = 0;
+		int end = Math.min(index + len, in.length());
+		for (int i = off; i < end; i++, n++) {
+			buf[i] = in.charAt(index++);
+		}
+		return n;
+	}
+
+	@Override
+	public boolean markSupported()
+	{
+		return true;
+	}
+
+	@Override
+	public void mark(int readLimit)
+	{
+		Parameters.checkCondition(readLimit >= 0);
+		mark = index;
+	}
+
+	@Override
+	public void reset()
+	{
+		index = mark;
+	}
+
+	@Override
+	public long skip(long n)
+	{
+		Parameters.checkCondition(n >= 0);
+		if (finished()) {
+			return 0;
+		}
+		long skipped = n;
+		if (n > in.length() - index) {
+			skipped = in.length() - index;
+		}
+		index += skipped;
+		return skipped;
+	}
+
+	@Override
+	public void close()
+	{
+		/* ... */
+	}
+
+	private boolean finished()
+	{
+		return index >= in.length();
+	}
+}
---
+++ test/org/pitaya/io/StringReaderTest.java
@@ -0,0 +1,93 @@
+/*----------------------------------------------------------------------------*
+ * This file is part of Pitaya.                                               *
+ * Copyright (C) 2012 Osman KOCAK <kocakosm@gmail.com>                        *
+ *                                                                            *
+ * This program is free software: you can redistribute it and/or modify it    *
+ * under the terms of the GNU Lesser General Public License as published by   *
+ * the Free Software Foundation, either version 3 of the License, or (at your *
+ * option) any later version.                                                 *
+ * This program is distributed in the hope that it will be useful, but        *
+ * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY *
+ * or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public     *
+ * License for more details.                                                  *
+ * You should have received a copy of the GNU Lesser General Public License   *
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.       *
+ *----------------------------------------------------------------------------*/
+
+package org.pitaya.io;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+/**
+ * {@link StringReader} test.
+ *
+ * @author Osman KOCAK
+ */
+public final class StringReaderTest
+{
+	@Test
+	public void testReady()
+	{
+		StringReader reader = new StringReader("");
+		assertTrue(reader.ready());
+	}
+
+	@Test
+	public void testRead()
+	{
+		StringReader reader = new StringReader("Hello", " ", "world !");
+		for (char c : "Hello world !".toCharArray()) {
+			assertEquals(c, reader.read());
+		}
+		assertEquals(-1, reader.read());
+	}
+
+	@Test
+	public void testReadArray()
+	{
+		StringReader reader = new StringReader("Hello", " ", "world !");
+		char[] buf = new char[5];
+		assertEquals(5, reader.read(buf));
+		assertArrayEquals("Hello".toCharArray(), buf);
+
+		reader = new StringReader("Hello");
+		buf = new char[15];
+		assertEquals(5, reader.read(buf));
+		for (int i = 0; i < "Hello".length(); i++) {
+			assertEquals("Hello".charAt(i), buf[i]);
+		}
+	}
+
+	@Test
+	public void testSkip()
+	{
+		StringReader reader = new StringReader("Hello", " ", "world !");
+		assertEquals(6, reader.skip(6));
+		for (char c : "world !".toCharArray()) {
+			assertEquals(c, reader.read());
+		}
+		assertEquals(0, reader.skip(1));
+		assertEquals(-1, reader.read());
+
+		reader = new StringReader("Hello");
+		assertEquals(5, reader.skip(6));
+		assertEquals(-1, reader.read());
+		assertEquals(0, reader.skip(1));
+	}
+
+	@Test
+	public void testMarkAndReset()
+	{
+		StringReader reader = new StringReader("Hello", " ", "world !");
+		assertTrue(reader.markSupported());
+		reader.mark(5);
+		char[] buf = new char[5];
+		reader.read(buf);
+		assertArrayEquals("Hello".toCharArray(), buf);
+		reader.reset();
+		reader.read(buf);
+		assertArrayEquals("Hello".toCharArray(), buf);
+	}
+}
