930c29ac915d9e504bae8fffcb31440147b50bf2
Osman KOCAK <kocakosm@gmail.com>
2014-02-22 12:49:49 +0100

    Added utilities for Throwables

---
+++ test/org/pitaya/util/ThrowablesTest.java
@@ -0,0 +1,73 @@
+/*----------------------------------------------------------------------------*
+ * This file is part of Pitaya.                                               *
+ * Copyright (C) 2012-2014 Osman KOCAK <kocakosm@gmail.com>                   *
+ *                                                                            *
+ * This program is free software: you can redistribute it and/or modify it    *
+ * under the terms of the GNU Lesser General Public License as published by   *
+ * the Free Software Foundation, either version 3 of the License, or (at your *
+ * option) any later version.                                                 *
+ * This program is distributed in the hope that it will be useful, but        *
+ * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY *
+ * or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public     *
+ * License for more details.                                                  *
+ * You should have received a copy of the GNU Lesser General Public License   *
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.       *
+ *----------------------------------------------------------------------------*/
+
+package org.pitaya.util;
+
+import static org.junit.Assert.*;
+
+import java.util.Arrays;
+import java.util.List;
+
+import org.junit.Test;
+
+/**
+ * {@link Throwables}' unit tests.
+ *
+ * @author Osman KOCAK
+ */
+public final class ThrowablesTest
+{
+	@Test
+	public void testGetRootCause()
+	{
+		Throwable root = new Throwable();
+		assertEquals(root, Throwables.getRootCause(root));
+
+		Throwable t = new Throwable(new Throwable(root));
+		assertEquals(root, Throwables.getRootCause(t));
+	}
+
+	@Test
+	public void testGetChain()
+	{
+		Throwable root = new Throwable();
+		assertEquals(Arrays.asList(root), Throwables.getChain(root));
+
+		Throwable t = new Throwable(new Throwable(root));
+		assertEquals(Arrays.asList(t, t.getCause(), root), Throwables.getChain(t));
+	}
+
+	@Test
+	public void testGetStackTrace()
+	{
+		Throwable t = new Throwable("ERROR");
+		String trace = Throwables.getStackTrace(t);
+		assertTrue(trace.startsWith("java.lang.Throwable: ERROR"));
+		assertTrue(trace.length() > "java.lang.Throwable: ERROR".length());
+	}
+
+	@Test
+	public void testGetStackFrames()
+	{
+		Throwable t = new Throwable(new NullPointerException("NPE"));
+		StackTraceElement[] elements = t.getStackTrace();
+		List<String> frames = Throwables.getStackFrames(t);
+		assertEquals(elements.length, frames.size());
+		for (int i = 0; i < elements.length; i++) {
+			assertEquals(elements[i].toString(), frames.get(i));
+		}
+	}
+}
