638e2e95f109854590b44248249a018d321fff3d
Osman KOCAK <kocakosm@gmail.com>
2015-03-26 00:01:06 +0100

    Added TextFiles

---
+++ src/org/kocakosm/pitaya/io/TextFiles.java
@@ -0,0 +1,436 @@
+/*----------------------------------------------------------------------------*
+ * This file is part of Pitaya.                                               *
+ * Copyright (C) 2012-2015 Osman KOCAK <kocakosm@gmail.com>                   *
+ *                                                                            *
+ * This program is free software: you can redistribute it and/or modify it    *
+ * under the terms of the GNU Lesser General Public License as published by   *
+ * the Free Software Foundation, either version 3 of the License, or (at your *
+ * option) any later version.                                                 *
+ * This program is distributed in the hope that it will be useful, but        *
+ * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY *
+ * or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public     *
+ * License for more details.                                                  *
+ * You should have received a copy of the GNU Lesser General Public License   *
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.       *
+ *----------------------------------------------------------------------------*/
+
+package org.kocakosm.pitaya.io;
+
+import org.kocakosm.pitaya.charset.Charsets;
+import org.kocakosm.pitaya.util.Parameters;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.nio.charset.Charset;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.LinkedList;
+import java.util.List;
+
+/**
+ * Text files utilities.
+ *
+ * @see XFiles
+ *
+ * @author Osman KOCAK
+ */
+public final class TextFiles
+{
+	/**
+	 * Returns the first (up to 10) lines of the given {@code File} using
+	 * the system's default charset. Named after the Unix command of the
+	 * same name.
+	 *
+	 * @param f the {@code File} to read.
+	 *
+	 * @return the first lines of the given {@code File}.
+	 *
+	 * @throws NullPointerException if {@code f} is {@code null}.
+	 * @throws IOException if {@code f} does not exist, or if it is a
+	 *	directory rather than a regular file, or if it can't be read.
+	 * @throws SecurityException if a security manager exists and denies
+	 *	read access to {@code f}.
+	 */
+	public static List<String> head(File f) throws IOException
+	{
+		return head(f, Charsets.DEFAULT);
+	}
+
+	/**
+	 * Returns the first (up to 10) lines of the given {@code File} using
+	 * the specified charset. Named after the Unix command of the same name.
+	 *
+	 * @param f the {@code File} to read.
+	 * @param charset the charset to use.
+	 *
+	 * @return the first lines of the given {@code File}.
+	 *
+	 * @throws NullPointerException if {@code f} is {@code null}.
+	 * @throws IOException if {@code f} does not exist, or if it is a
+	 *	directory rather than a regular file, or if it can't be read.
+	 * @throws SecurityException if a security manager exists and denies
+	 *	read access to {@code f}.
+	 */
+	public static List<String> head(File f, Charset charset) throws IOException
+	{
+		return head(f, 10, charset);
+	}
+
+	/**
+	 * Returns the first (up to {@code n}) lines of the given {@code File}
+	 * using the system's default charset. Named after the Unix command of
+	 * the same name.
+	 *
+	 * @param f the {@code File} to read.
+	 * @param n the maximum number of lines to read.
+	 *
+	 * @return the first lines of the given {@code File}.
+	 *
+	 * @throws NullPointerException if {@code f} is {@code null}.
+	 * @throws IllegalArgumentException if {@code n} is negative.
+	 * @throws IOException if {@code f} does not exist, or if it is a
+	 *	directory rather than a regular file, or if it can't be read.
+	 * @throws SecurityException if a security manager exists and denies
+	 *	read access to {@code f}.
+	 */
+	public static List<String> head(File f, int n) throws IOException
+	{
+		return head(f, n, Charsets.DEFAULT);
+	}
+
+	/**
+	 * Returns the first (up to {@code n}) lines of the given {@code File}
+	 * using the specified charset. Named after the Unix command of the same
+	 * name.
+	 *
+	 * @param f the {@code File} to read.
+	 * @param n the maximum number of lines to read.
+	 * @param charset the charset to use.
+	 *
+	 * @return the first lines of the given {@code File}.
+	 *
+	 * @throws NullPointerException if one of the arguments is {@code null}.
+	 * @throws IllegalArgumentException if {@code n} is negative.
+	 * @throws IOException if {@code f} does not exist, or if it is a
+	 *	directory rather than a regular file, or if it can't be read.
+	 * @throws SecurityException if a security manager exists and denies
+	 *	read access to {@code f}.
+	 */
+	public static List<String> head(File f, int n, Charset charset)
+		throws IOException
+	{
+		Parameters.checkCondition(n >= 0);
+		List<String> lines = new ArrayList<String>();
+		BufferedReader reader = newReader(f, charset);
+		try {
+			String line = reader.readLine();
+			while (line != null && lines.size() < n) {
+				lines.add(line);
+				line = reader.readLine();
+			}
+		} finally {
+			IO.close(reader);
+		}
+		return Collections.unmodifiableList(lines);
+	}
+
+	/**
+	 * Returns the last (up to 10) lines of the given {@code File} using the
+	 * system's default charset. Named after the Unix command of the same
+	 * name.
+	 *
+	 * @param f the {@code File} to read.
+	 *
+	 * @return the last lines of the given {@code File}.
+	 *
+	 * @throws NullPointerException if {@code f} is {@code null}.
+	 * @throws IOException if {@code f} does not exist, or if it is a
+	 *	directory rather than a regular file, or if it can't be read.
+	 * @throws SecurityException if a security manager exists and denies
+	 *	read access to {@code f}.
+	 */
+	public static List<String> tail(File f) throws IOException
+	{
+		return tail(f, Charsets.DEFAULT);
+	}
+
+	/**
+	 * Returns the last (up to 10) lines of the given {@code File} using the
+	 * specified charset. Named after the Unix command of the same name.
+	 *
+	 * @param f the {@code File} to read.
+	 * @param charset the charset to use.
+	 *
+	 * @return the last lines of the given {@code File}.
+	 *
+	 * @throws NullPointerException if one of the arguments is {@code null}.
+	 * @throws IOException if {@code f} does not exist, or if it is a
+	 *	directory rather than a regular file, or if it can't be read.
+	 * @throws SecurityException if a security manager exists and denies
+	 *	read access to {@code f}.
+	 */
+	public static List<String> tail(File f, Charset charset) throws IOException
+	{
+		return tail(f, 10, charset);
+	}
+
+	/**
+	 * Returns the last (up to n) lines of the given {@code File} using the
+	 * system's default charset. Named after the Unix command of the same
+	 * name.
+	 *
+	 * @param f the {@code File} to read.
+	 * @param n the maximum number of lines to read.
+	 *
+	 * @return the last lines of the given {@code File}.
+	 *
+	 * @throws NullPointerException if {@code f} is {@code null}.
+	 * @throws IllegalArgumentException if {@code n} is negative.
+	 * @throws IOException if {@code f} does not exist, or if it is a
+	 *	directory rather than a regular file, or if it can't be read.
+	 * @throws SecurityException if a security manager exists and denies
+	 *	read access to {@code f}.
+	 */
+	public static List<String> tail(File f, int n) throws IOException
+	{
+		return tail(f, n, Charsets.DEFAULT);
+	}
+
+	/**
+	 * Returns the last (up to n) lines of the given {@code File} using the
+	 * specified charset. Named after the Unix command of the same name.
+	 *
+	 * @param f the {@code File} to read.
+	 * @param n the maximum number of lines to read.
+	 * @param charset the charset to use.
+	 *
+	 * @return the last lines of the given {@code File}.
+	 *
+	 * @throws NullPointerException if one of the arguments is {@code null}.
+	 * @throws IllegalArgumentException if {@code n} is negative.
+	 * @throws IOException if {@code f} does not exist, or if it is a
+	 *	directory rather than a regular file, or if it can't be read.
+	 * @throws SecurityException if a security manager exists and denies
+	 *	read access to {@code f}.
+	 */
+	public static List<String> tail(File f, int n, Charset charset)
+		throws IOException
+	{
+		Parameters.checkCondition(n >= 0);
+		if (n == 0) {
+			return Collections.emptyList();
+		}
+		List<String> lines = new LinkedList<String>();
+		BufferedReader reader = newReader(f, charset);
+		try {
+			String line = reader.readLine();
+			while (line != null) {
+				lines.add(line);
+				if (lines.size() > n) {
+					lines.remove(0);
+				}
+				line = reader.readLine();
+			}
+		} finally {
+			IO.close(reader);
+		}
+		return Collections.unmodifiableList(lines);
+	}
+
+	/**
+	 * Returns a new {@code BufferedReader} to read the given {@code File}
+	 * using the system's default charset.
+	 *
+	 * @param f the file to read from.
+	 *
+	 * @return a {@code BufferedReader} to read the given {@code File}.
+	 *
+	 * @throws NullPointerException if {@code f} is {@code null}.
+	 * @throws FileNotFoundException if {@code f} doesn't exist, or if it is
+	 *	a directory rather than a regular file, or if it can't be opened
+	 *	for reading.
+	 * @throws SecurityException if a security manager exists and denies
+	 *	read access to {@code f}.
+	 */
+	public static BufferedReader newReader(File f) throws FileNotFoundException
+	{
+		return newReader(f, Charsets.DEFAULT);
+	}
+
+	/**
+	 * Returns a new {@code BufferedReader} to read the given {@code File}
+	 * using the specified charset.
+	 *
+	 * @param f the file to read from.
+	 * @param charset the charset to use.
+	 *
+	 * @return a {@code BufferedReader} to read the given {@code File}.
+	 *
+	 * @throws NullPointerException if one of the arguments is {@code null}.
+	 * @throws FileNotFoundException if {@code f} doesn't exist, or if it is
+	 *	a directory rather than a regular file, or if it can't be opened
+	 *	for reading.
+	 * @throws SecurityException if a security manager exists and denies
+	 *	read access to {@code f}.
+	 */
+	public static BufferedReader newReader(File f, Charset charset)
+		throws FileNotFoundException
+	{
+		InputStream in = new FileInputStream(f);
+		return new BufferedReader(new InputStreamReader(in, charset));
+	}
+
+	/**
+	 * Returns a new {@code BufferedWriter} to write to the given
+	 * {@code File} using the system's default charset.
+	 *
+	 * @param f the file to write to.
+	 * @param options the write options.
+	 *
+	 * @return a {@code BufferedWriter} to write to the given {@code File}.
+	 *
+	 * @throws NullPointerException if one of the arguments is {@code null}.
+	 * @throws IllegalArgumentException if incompatible options are given.
+	 * @throws FileNotFoundException if {@code f} exists but is a directory
+	 *	rather than a regular file, or if it does not exist but cannot
+	 *	be created, or if it cannot be opened for any other reason.
+	 * @throws IOException if the {@link WriteOption#CREATE_NEW} option is
+	 *	given and the specified file already exists.
+	 * @throws SecurityException if a security manager exists and denies
+	 *	write access to {@code f}.
+	 */
+	public static BufferedWriter newWriter(File f, WriteOption... options)
+		throws IOException
+	{
+		return newWriter(f, Charsets.DEFAULT, options);
+	}
+
+	/**
+	 * Returns a new {@code BufferedWriter} to write to the given
+	 * {@code File} using the specified charset.
+	 *
+	 * @param f the file to write to.
+	 * @param charset the charset to use.
+	 * @param options the write options.
+	 *
+	 * @return a {@code BufferedWriter} to write to the given {@code File}.
+	 *
+	 * @throws NullPointerException if one of the arguments is {@code null}.
+	 * @throws IllegalArgumentException if incompatible options are given.
+	 * @throws FileNotFoundException if {@code f} exists but is a directory
+	 *	rather than a regular file, or if it does not exist but cannot
+	 *	be created, or if it cannot be opened for any other reason.
+	 * @throws IOException if the {@link WriteOption#CREATE_NEW} option is
+	 *	given and the specified file already exists.
+	 * @throws SecurityException if a security manager exists and denies
+	 *	write access to {@code f}.
+	 */
+	public static BufferedWriter newWriter(File f, Charset charset,
+		WriteOption... options) throws IOException
+	{
+		OutputStream out = XFiles.newOutputStream(f, options);
+		return new BufferedWriter(new OutputStreamWriter(out, charset));
+	}
+
+	/**
+	 * Reads the whole content of the given {@code File} as a {@code String}
+	 * using the system's default charset.
+	 *
+	 * @param f the file to read.
+	 *
+	 * @return the file's content as a {@code String}.
+	 *
+	 * @throws NullPointerException if {@code f} is {@code null}.
+	 * @throws IOException if {@code f} does not exist, or if it is a
+	 *	directory rather than a regular file, or if it can't be read.
+	 * @throws SecurityException if a security manager exists and denies
+	 *	read access to {@code f}.
+	 */
+	public static String read(File f) throws IOException
+	{
+		return read(f, Charsets.DEFAULT);
+	}
+
+	/**
+	 * Reads the whole content of the given {@code File} as a {@code String}
+	 * using the specified charset.
+	 *
+	 * @param f the file to read.
+	 * @param charset the charset to use.
+	 *
+	 * @return the file's content as a {@code String}.
+	 *
+	 * @throws NullPointerException if one of the arguments is {@code null}.
+	 * @throws IOException if {@code f} does not exist, or if it is a
+	 *	directory rather than a regular file, or if it can't be read.
+	 * @throws SecurityException if a security manager exists and denies
+	 *	read access to {@code f}.
+	 */
+	public static String read(File f, Charset charset) throws IOException
+	{
+		BufferedReader in = newReader(f, charset);
+		try {
+			return CharStreams.read(in);
+		} finally {
+			IO.close(in);
+		}
+	}
+
+	/**
+	 * Reads all the lines from the given {@code File} using the system's
+	 * default charset. Note that the returned {@code List} is immutable.
+	 *
+	 * @param f the file to read.
+	 *
+	 * @return the file's lines.
+	 *
+	 * @throws NullPointerException if {@code f} is {@code null}.
+	 * @throws IOException if {@code f} does not exist, or if it is a
+	 *	directory rather than a regular file, or if it can't be read.
+	 * @throws SecurityException if a security manager exists and denies
+	 *	read access to {@code f}.
+	 */
+	public static List<String> readLines(File f) throws IOException
+	{
+		return readLines(f, Charsets.DEFAULT);
+	}
+
+	/**
+	 * Reads all the lines from the given {@code File} using the specified
+	 * charset. Note that the returned {@code List} is immutable.
+	 *
+	 * @param f the file to read.
+	 * @param charset the charset to use.
+	 *
+	 * @return the file's lines.
+	 *
+	 * @throws NullPointerException if one of the arguments is {@code null}.
+	 * @throws IOException if {@code f} does not exist, or if it is a
+	 *	directory rather than a regular file, or if it can't be read.
+	 * @throws SecurityException if a security manager exists and denies
+	 *	read access to {@code f}.
+	 */
+	public static List<String> readLines(File f, Charset charset)
+		throws IOException
+	{
+		BufferedReader in = newReader(f, charset);
+		try {
+			return CharStreams.readLines(in);
+		} finally {
+			IO.close(in);
+		}
+	}
+
+	private TextFiles()
+	{
+		/* ... */
+	}
+}
---
+++ test/org/kocakosm/pitaya/io/TextFilesTest.java
@@ -0,0 +1,260 @@
+/*----------------------------------------------------------------------------*
+ * This file is part of Pitaya.                                               *
+ * Copyright (C) 2012-2015 Osman KOCAK <kocakosm@gmail.com>                   *
+ *                                                                            *
+ * This program is free software: you can redistribute it and/or modify it    *
+ * under the terms of the GNU Lesser General Public License as published by   *
+ * the Free Software Foundation, either version 3 of the License, or (at your *
+ * option) any later version.                                                 *
+ * This program is distributed in the hope that it will be useful, but        *
+ * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY *
+ * or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public     *
+ * License for more details.                                                  *
+ * You should have received a copy of the GNU Lesser General Public License   *
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.       *
+ *----------------------------------------------------------------------------*/
+
+package org.kocakosm.pitaya.io;
+
+import static org.junit.Assert.*;
+
+import org.kocakosm.pitaya.util.Strings;
+import org.kocakosm.pitaya.util.XArrays;
+
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.io.Reader;
+import java.io.Writer;
+import java.util.Arrays;
+import java.util.List;
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.TemporaryFolder;
+
+/**
+ * {@link TextFiles}' unit tests.
+ *
+ * @author Osman KOCAK
+ */
+public final class TextFilesTest
+{
+	private static final char[] ALPHABET = "0123456789".toCharArray();
+
+	@Rule
+	public TemporaryFolder tmp = new TemporaryFolder();
+
+	@Test
+	public void testHead() throws Exception
+	{
+		File test = tmp.newFolder();
+		String[] lines = randomStrings(15);
+		File src = createFile(test, "data", lines);
+		List<String> head = TextFiles.head(src);
+		assertArrayEquals(Arrays.copyOf(lines, 10), head.toArray());
+	}
+
+	@Test
+	public void testHeadFirstLine() throws Exception
+	{
+		File test = tmp.newFolder();
+		String[] lines = randomStrings(5);
+		File src = createFile(test, "data", lines);
+		List<String> head = TextFiles.head(src, 1);
+		assertArrayEquals(Arrays.copyOf(lines, 1), head.toArray());
+	}
+
+	@Test
+	public void testTail() throws Exception
+	{
+		File test = tmp.newFolder();
+		String[] lines = randomStrings(15);
+		File src = createFile(test, "data", lines);
+		List<String> head = TextFiles.tail(src);
+		assertArrayEquals(XArrays.copyOf(lines, 5, 10), head.toArray());
+	}
+
+	@Test
+	public void testTailLastLine() throws Exception
+	{
+		File test = tmp.newFolder();
+		String[] lines = randomStrings(5);
+		File src = createFile(test, "data", lines);
+		List<String> head = TextFiles.tail(src, 1);
+		assertArrayEquals(XArrays.copyOf(lines, 4, 1), head.toArray());
+	}
+
+	@Test
+	public void testNewReader() throws Exception
+	{
+		File test = tmp.newFolder();
+		File txt = createFile(test, "hello.txt", "Hello");
+		assertEquals("Hello", read(TextFiles.newReader(txt)));
+	}
+
+	@Test
+	public void testNewWriter() throws Exception
+	{
+		File test = tmp.newFolder();
+		File txt = createFile(test, "hello.txt", "Hello");
+		Writer writer = TextFiles.newWriter(txt);
+		writer.write("World");
+		writer.flush();
+		writer.close();
+		assertEquals("World", read(txt));
+	}
+
+	@Test
+	public void testNewWriterWithAppendOption() throws Exception
+	{
+		File test = tmp.newFolder();
+		File txt = createFile(test, "hello.txt", "Hello");
+		Writer writer = TextFiles.newWriter(txt, WriteOption.APPEND);
+		writer.write(" ");
+		writer.write("World");
+		writer.flush();
+		writer.close();
+		assertEquals("Hello World", read(txt));
+	}
+
+	@Test
+	public void testNewWriterWithOverwriteOption() throws Exception
+	{
+		File test = tmp.newFolder();
+		File txt = createFile(test, "hello.txt", "Kello");
+		Writer writer = TextFiles.newWriter(txt, WriteOption.OVERWRITE);
+		writer.write("H");
+		writer.flush();
+		writer.close();
+		assertEquals("Hello", read(txt));
+	}
+
+	@Test
+	public void testNewWriterWithCreateOption() throws Exception
+	{
+		File test = tmp.newFolder();
+		File f = new File(test, "hello.txt");
+		Writer writer = TextFiles.newWriter(f, WriteOption.CREATE);
+		writer.write("Hello");
+		writer.flush();
+		writer.close();
+		assertEquals("Hello", read(f));
+	}
+
+	@Test(expected = IOException.class)
+	public void testNewWriterWithCreateOptionOnExistingFile()
+		throws Exception
+	{
+		File test = tmp.newFolder();
+		File txt = createFile(test, "hello.txt", "Hello");
+		TextFiles.newWriter(txt, WriteOption.CREATE);
+	}
+
+	@Test
+	public void testNewWriterWithUpdateOption() throws Exception
+	{
+		File test = tmp.newFolder();
+		File txt = createFile(test, "hello.txt", "Test");
+		Writer out = TextFiles.newWriter(txt, WriteOption.UPDATE);
+		out.write("Hello");
+		out.flush();
+		out.close();
+		assertEquals("Hello", read(txt));
+	}
+
+	@Test(expected = FileNotFoundException.class)
+	public void testNewWriterWithUpdateOptionOnMissingFile() throws Exception
+	{
+		File test = tmp.newFolder();
+		File f = new File(test, "hello.txt");
+		TextFiles.newWriter(f, WriteOption.UPDATE);
+	}
+
+	@Test(expected = IllegalArgumentException.class)
+	public void testNewWriterWithUpdateAndCreateOptions() throws Exception
+	{
+		File test = tmp.newFolder();
+		File f = new File(test, "hello.txt");
+		TextFiles.newWriter(f, WriteOption.UPDATE, WriteOption.CREATE);
+	}
+
+	@Test(expected = IllegalArgumentException.class)
+	public void testNewWriterWithOverwriteAndAppendOptions() throws Exception
+	{
+		File test = tmp.newFolder();
+		File f = new File(test, "hello.txt");
+		TextFiles.newWriter(f, WriteOption.OVERWRITE, WriteOption.APPEND);
+	}
+
+	@Test
+	public void testRead() throws Exception
+	{
+		File test = tmp.newFolder();
+		File txt = createFile(test, "hello.txt", "Hello");
+		assertEquals("Hello", TextFiles.read(txt));
+	}
+
+	@Test
+	public void testReadLines() throws Exception
+	{
+		File test = tmp.newFolder();
+		File txt = createFile(test, "hello.txt", "Hello", "World");
+		assertEquals(Arrays.asList("Hello", "World"), TextFiles.readLines(txt));
+	}
+
+	private String[] randomStrings(int count)
+	{
+		String[] data = new String[count];
+		for (int i = 0; i < count; i++) {
+			data[i] = Strings.random(count, ALPHABET);
+		}
+		return data;
+	}
+
+	private File createFile(File dir, String name, String... lines)
+		throws Exception
+	{
+		File f = new File(dir, name);
+		BufferedWriter writer = null;
+		try {
+			OutputStream out = new FileOutputStream(f);
+			writer = new BufferedWriter(new OutputStreamWriter(out));
+			writer.write(lines[0]);
+			for (int i = 1; i < lines.length; i++) {
+				writer.newLine();
+				writer.write(lines[i]);
+			}
+		} finally {
+			IO.flush(writer);
+			IO.close(writer);
+		}
+		return f;
+	}
+
+	private String read(File f) throws Exception
+	{
+		InputStream in = null;
+		try {
+			in = new FileInputStream(f);
+			return CharStreams.read(in);
+		} finally {
+			IO.close(in);
+		}
+	}
+
+	private String read(Reader in) throws Exception
+	{
+		try {
+			return CharStreams.read(in);
+		} finally {
+			IO.close(in);
+		}
+	}
+}
