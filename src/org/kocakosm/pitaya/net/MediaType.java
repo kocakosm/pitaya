6fa1427c2e173c6ec0ba119de41cbcccec0f883e
Osman KOCAK <kocakosm@gmail.com>
2015-08-02 22:29:35 +0200

    [#20] Added MediaType

---
+++ src/org/kocakosm/pitaya/net/MediaType.java
@@ -0,0 +1,502 @@
+/*----------------------------------------------------------------------------*
+ * This file is part of Pitaya.                                               *
+ * Copyright (C) 2012-2015 Osman KOCAK <kocakosm@gmail.com>                   *
+ *                                                                            *
+ * This program is free software: you can redistribute it and/or modify it    *
+ * under the terms of the GNU Lesser General Public License as published by   *
+ * the Free Software Foundation, either version 3 of the License, or (at your *
+ * option) any later version.                                                 *
+ * This program is distributed in the hope that it will be useful, but        *
+ * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY *
+ * or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public     *
+ * License for more details.                                                  *
+ * You should have received a copy of the GNU Lesser General Public License   *
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.       *
+ *----------------------------------------------------------------------------*/
+
+package org.kocakosm.pitaya.net;
+
+import org.kocakosm.pitaya.charset.ASCII;
+import org.kocakosm.pitaya.collection.ImmutableMap;
+import org.kocakosm.pitaya.util.Joiner;
+import org.kocakosm.pitaya.util.Parameters;
+import org.kocakosm.pitaya.util.Strings;
+import org.kocakosm.pitaya.util.XObjects;
+
+import java.io.Serializable;
+import java.nio.charset.Charset;
+import java.nio.charset.IllegalCharsetNameException;
+import java.nio.charset.UnsupportedCharsetException;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Map.Entry;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+/**
+ * Represents an Internet Media Type. This class also supports the concept of
+ * media ranges defined by HTTP/1.1. The {@code *} character is treated as a
+ * wildcard and is used to represent any acceptable type or subtype value. All
+ * values for type, subtype, parameter attributes or parameter values must be
+ * valid according to RFCs 2045 and 2046. All portions of the media type that
+ * are case-insensitive (type, subtype, parameter attributes) are normalized to
+ * lowercase. Parameters' values are not modified except for the {@code charset}
+ * parameter which is normalised to uppercase. Instances of this class are
+ * immutable.
+ *
+ * @see http://www.ietf.org/rfc/rfc2045.txt
+ * @see http://www.ietf.org/rfc/rfc2046.txt
+ * @see http://www.w3.org/Protocols/rfc2616/rfc2616-sec14.html#sec14.1
+ *
+ * @since 0.3
+ *
+ * @author Osman KOCAK
+ */
+public final class MediaType implements Serializable
+{
+	private static final long serialVersionUID = 30386013071308392L;
+	private static final String CHARSET;
+	private static final String WILDCARD;
+	private static final Joiner PARAMETERS_JOINER;
+	private static final Joiner ATTRIBUTE_VALUE_JOINER;
+	private static final Pattern TOKEN_PATTERN;
+	private static final Pattern MEDIA_TYPE_PATTERN;
+	static {
+		WILDCARD = "*";
+		CHARSET = "charset";
+		ATTRIBUTE_VALUE_JOINER = Joiner.on("=");
+		PARAMETERS_JOINER = Joiner.on("; ").withPrefix("; ");
+		String token = "[\\p{ASCII}&&[^\\p{Cntrl}\\s\\(\\)<>@,;:\"/\\[\\]\\?=\\\\]]+";
+		TOKEN_PATTERN = Pattern.compile(token);
+		String quotedString = "\"([\\p{ASCII}&&[^\"\\\\]]|\\\\\\p{ASCII})*\"";
+		String regex = Strings.concat("(", token, ")/(", token, ")",
+			"((;[ \t\r\n]*", token, "=(", token, "|", quotedString, "))*)");
+		MEDIA_TYPE_PATTERN = Pattern.compile(regex);
+	}
+
+	/**
+	 * Range that matches all media types.
+	 *
+	 * @see #is(MediaType)
+	 */
+	public static final MediaType ANY_TYPE = new MediaType(WILDCARD, WILDCARD);
+
+	/**
+	 * Range that matches all text types.
+	 *
+	 * @see #is(MediaType)
+	 */
+	public static final MediaType ANY_TEXT_TYPE = new MediaType("text", WILDCARD);
+
+	/**
+	 * Range that matches all image types.
+	 *
+	 * @see #is(MediaType)
+	 */
+	public static final MediaType ANY_IMAGE_TYPE = new MediaType("image", WILDCARD);
+
+	/**
+	 * Range that matches all audio types.
+	 *
+	 * @see #is(MediaType)
+	 */
+	public static final MediaType ANY_AUDIO_TYPE = new MediaType("audio", WILDCARD);
+
+	/**
+	 * Range that matches all video types.
+	 *
+	 * @see #is(MediaType)
+	 */
+	public static final MediaType ANY_VIDEO_TYPE = new MediaType("video", WILDCARD);
+
+	/**
+	 * Range that matches all application types.
+	 *
+	 * @see #is(MediaType)
+	 */
+	public static final MediaType ANY_APPLICATION_TYPE = new MediaType("application", WILDCARD);
+
+	/** Image/jpeg. */
+	public static final MediaType JPEG = new MediaType("image", "jpeg");
+
+	/** Image/png. */
+	public static final MediaType PNG = new MediaType("image", "png");
+
+	/** Image/gif. */
+	public static final MediaType GIF = new MediaType("image", "gif");
+
+	/** Text/css. */
+	public static final MediaType CSS = new MediaType("text", "css");
+
+	/** Text/html. */
+	public static final MediaType HTML = new MediaType("text", "html");
+
+	/** Text/plain. */
+	public static final MediaType PLAIN_TEXT = new MediaType("text", "plain");
+
+	/** Application/json. */
+	public static final MediaType JSON = new MediaType("application", "json");
+
+	/** Application/xml. */
+	public static final MediaType XML = new MediaType("application", "xml");
+
+	/** Application/octet-stream. */
+	public static final MediaType OCTET_STREAM = new MediaType("application", "octet-stream");
+
+	/**
+	 * Parses a {@code MediaType} from its {@code String} representation.
+	 *
+	 * @param input the input {@code String} to be parsed.
+	 *
+	 * @return the parsed {@code MediaType} instance.
+	 *
+	 * @throws NullPointerException if {@code input} is {@code null}.
+	 * @throws IllegalArgumentException if {@code input} can't be parsed or
+	 *	if a wildcard is used for the type, but not for the subtype.
+	 */
+	public static MediaType parse(String input)
+	{
+		Parameters.checkNotNull(input);
+		Matcher m = MEDIA_TYPE_PATTERN.matcher(input);
+		Parameters.checkCondition(m.matches());
+		String type = m.group(1);
+		String subtype = m.group(2);
+		String params = m.group(3);
+		ParametersParser parser = new ParametersParser();
+		return create(type, subtype, parser.parse(params));
+	}
+
+	/**
+	 * Creates a new {@code MediaType} with the given type and subtype.
+	 *
+	 * @param type the type.
+	 * @param subtype the subtype.
+	 *
+	 * @return the created {@code MediaType} instance.
+	 *
+	 * @throws NullPointerException if one of the arguments is {@code null}.
+	 * @throws IllegalArgumentException if type or subtype is invalid or if
+	 *	a wildcard is used for the type, but not for the subtype.
+	 */
+	public static MediaType create(String type, String subtype)
+	{
+		Map<String, String> params = Collections.emptyMap();
+		return create(type, subtype, params);
+	}
+
+	private static MediaType create(String type, String subtype,
+		Map<String, String> parameters)
+	{
+		Parameters.checkCondition(!WILDCARD.equals(type) || WILDCARD.equals(subtype));
+		String normalizedType = normalizeToken(type);
+		String normalizedSubtype = normalizeToken(subtype);
+		Map<String, String> params = normalizeParameters(parameters);
+		return new MediaType(normalizedType, normalizedSubtype, params);
+	}
+
+	private static String normalizeToken(String token)
+	{
+		Parameters.checkNotNull(token);
+		Parameters.checkCondition(TOKEN_PATTERN.matcher(token).matches());
+		return ASCII.toLowerCase(token);
+	}
+
+	private static Map<String, String> normalizeParameters(Map<String, String> parameters)
+	{
+		Map<String, String> params = new LinkedHashMap<String, String>();
+		for (Entry<String, String> entry : parameters.entrySet()) {
+			String attribute = normalizeToken(entry.getKey());
+			String value = CHARSET.equals(attribute)
+				? ASCII.toUpperCase(entry.getValue())
+				: Parameters.checkNotNull(entry.getValue());
+			params.put(attribute, value);
+		}
+		return params;
+	}
+
+	private final String type;
+	private final String subtype;
+	private final Map<String, String> parameters;
+
+	private MediaType(String type, String subtype)
+	{
+		this(type, subtype, Collections.<String, String>emptyMap());
+	}
+
+	private MediaType(String type, String subtype, Map<String, String> parameters)
+	{
+		this.type = type;
+		this.subtype = subtype;
+		this.parameters = ImmutableMap.copyOf(parameters);
+	}
+
+	/**
+	 * Returns the top-level media type.
+	 *
+	 * @return the top-level media type.
+	 */
+	public String type()
+	{
+		return type;
+	}
+
+	/**
+	 * Returns the media subtype.
+	 *
+	 * @return the media subtype.
+	 */
+	public String subtype()
+	{
+		return subtype;
+	}
+
+	/**
+	 * Returns the parameters of this media type. The returned {@code Map}
+	 * is immutable.
+	 *
+	 * @return the parameters of this media type.
+	 */
+	public Map<String, String> parameters()
+	{
+		return parameters;
+	}
+
+	/**
+	 * Returns whether the type or the subtype of this media type is the
+	 * wildcard.
+	 *
+	 * @return whether this media type has a wildcard.
+	 */
+	public boolean hasWildcard()
+	{
+		return WILDCARD.equals(type) || WILDCARD.equals(subtype);
+	}
+
+	/**
+	 * Returns the value of the charset parameter if it is specified, or
+	 * {@code null} otherwise.
+	 *
+	 * @return the charset parameter's value if present, or {@code null}
+	 *	otherwise.
+	 *
+	 * @throws IllegalCharsetNameException if the charset is illegal.
+	 * @throws UnsupportedCharsetException if the charset is not supported.
+	 */
+	public Charset charset()
+	{
+		String charset = parameters.get(CHARSET);
+		return charset == null ? null : Charset.forName(charset);
+	}
+
+	/**
+	 * Returns a new {@code MediaType} instance similar to this one but with
+	 * the {@code charset} parameter set to the {@link Charset#name name} of
+	 * the given charset.
+	 *
+	 * @param charset the charset.
+	 *
+	 * @return the created {@code MediaType}.
+	 *
+	 * @throws NullPointerException if {@code charset} is {@code null}.
+	 */
+	public MediaType withCharset(Charset charset)
+	{
+		return withParameter(CHARSET, charset.name());
+	}
+
+	/**
+	 * Returns a new {@code MediaType} instance similar to this one but with
+	 * the specified parameter set to the to the given value.
+	 *
+	 * @param attribute the attribute of the parameter to add.
+	 * @param value the value of the parameter to add.
+	 *
+	 * @return the created {@code MediaType}.
+	 *
+	 * @throws NullPointerException if one of the arguments is {@code null}.
+	 * @throws IllegalArgumentException if {@code attribute} is invalid.
+	 */
+	public MediaType withParameter(String attribute, String value)
+	{
+		Map<String, String> params = new HashMap<String, String>();
+		params.put(attribute, value);
+		return withParameters(params);
+	}
+
+	/**
+	 * Returns a new {@code MediaType} instance having the same type and
+	 * subtype as this one but whose parameters is the "union" of this
+	 * instances' parameters with the given ones (the given parameters have
+	 * precedence over this instance's parameters).
+	 *
+	 * @param parameters the parameters.
+	 *
+	 * @return the created {@code MediaType}.
+	 *
+	 * @throws NullPointerException if {@code parameters} is {@code null} or
+	 *	if it contains the {@code null} key or a {@code null} value.
+	 * @throws IllegalArgumentException if {@code parameters} contains an
+	 *	invalid attribute.
+	 */
+	public MediaType withParameters(Map<String, String> parameters)
+	{
+		Map<String, String> params = new LinkedHashMap<String, String>();
+		params.putAll(this.parameters);
+		params.putAll(normalizeParameters(parameters));
+		return new MediaType(type, subtype, params);
+	}
+
+	/**
+	 * Returns a new {@code MediaType} instance similar to this one but
+	 * without the specified parameter.
+	 *
+	 * @param attribute the attribute of the parameter to remove.
+	 *
+	 * @return the created {@code MediaType}.
+	 *
+	 * @throws NullPointerException if {@code attribute} is {@code null}.
+	 */
+	public MediaType withoutParameter(String attribute)
+	{
+		Parameters.checkNotNull(attribute);
+		Map<String, String> params = new LinkedHashMap<String, String>();
+		params.putAll(parameters);
+		params.remove(attribute);
+		return new MediaType(type, subtype, params);
+	}
+
+	/**
+	 * Returns a new {@code MediaType} instance with the same type and
+	 * subtype as this instance but without any parameters.
+	 *
+	 * @return the created {@code MediaType}.
+	 */
+	public MediaType withoutParameters()
+	{
+		return new MediaType(type, subtype);
+	}
+
+	/**
+	 * Returns whether this media type is within the specified media range.
+	 * Namely, it returns {@code true} if the type of {@code range} is the
+	 * wildcard or is equal to the type of this instance, and, if the subtype
+	 * of {@code range} is the wildcard or is equal to the subtype of this
+	 * instance, and, if all the parameters present in {@code range} are
+	 * also present in this instance.
+	 *
+	 * @param range the range to check this media type against.
+	 *
+	 * @return whether this media type is within the specified range.
+	 *
+	 * @throws NullPointerException if {@code range} is {@code null}.
+	 */
+	public boolean is(MediaType range)
+	{
+		return (range.type.equals(WILDCARD) || range.type.equals(type))
+			&& (range.subtype.equals(WILDCARD) || range.subtype.equals(subtype))
+			&& parameters.entrySet().containsAll(range.parameters.entrySet());
+	}
+
+	@Override
+	public boolean equals(Object o)
+	{
+		if (o == this) {
+			return true;
+		}
+		if (!(o instanceof MediaType)) {
+			return false;
+		}
+		MediaType mediaType = (MediaType) o;
+		return type.equals(mediaType.type)
+			&& subtype.equals(mediaType.subtype)
+			&& parameters.equals(mediaType.parameters);
+	}
+
+	@Override
+	public int hashCode()
+	{
+		return XObjects.hashCode(type, subtype, parameters);
+	}
+
+	@Override
+	public String toString()
+	{
+		if (parameters.isEmpty()) {
+			return Strings.concat(type, "/", subtype);
+		}
+		List<String> params = new ArrayList<String>();
+		for (Entry<String, String> parameter : parameters.entrySet()) {
+			String attribute = parameter.getKey();
+			String value = parameter.getValue();
+			if (!TOKEN_PATTERN.matcher(value).matches()) {
+				value = escapeAndQuote(value);
+			}
+			params.add(ATTRIBUTE_VALUE_JOINER.join(attribute, value));
+		}
+		return Strings.concat(type, "/", subtype, PARAMETERS_JOINER.join(params));
+	}
+
+	private String escapeAndQuote(String value)
+	{
+		StringBuilder escaped = new StringBuilder(value.length() * 2);
+		escaped.append('"');
+		for (char c : value.toCharArray()) {
+			if (c == '\\' || c == '"') {
+				escaped.append('\\');
+			}
+			escaped.append(c);
+		}
+		return escaped.append('"').toString();
+	}
+
+	private static final class ParametersParser
+	{
+		private String params;
+		private int index;
+
+		Map<String, String> parse(String params)
+		{
+			this.params = params;
+			Map<String, String> m = new LinkedHashMap<String, String>();
+			while (index < params.length() - 1) {
+				m.put(readAttribute(), readValue());
+			}
+			return m;
+		}
+
+		private String readAttribute()
+		{
+			StringBuilder attribute = new StringBuilder();
+			char c = params.charAt(++index);
+			while (c != '=') {
+				attribute.append(c);
+				c = params.charAt(++index);
+			}
+			return attribute.toString().trim();
+		}
+
+		private String readValue()
+		{
+			StringBuilder value = new StringBuilder();
+			boolean escaped = false;
+			char c = params.charAt(++index);
+			while (c != ';' || escaped) {
+				if (c == '\\' && !escaped) {
+					escaped = true;
+				} else {
+					value.append(c);
+				}
+				if (index == params.length() - 1) {
+					break;
+				}
+				c = params.charAt(++index);
+			}
+			boolean quoted = value.charAt(0) == '"';
+			return quoted ? value.substring(1, value.length() - 1) : value.toString();
+		}
+	}
+}
---
+++ test/org/kocakosm/pitaya/net/MediaTypeTest.java
@@ -0,0 +1,293 @@
+/*----------------------------------------------------------------------------*
+ * This file is part of Pitaya.                                               *
+ * Copyright (C) 2012-2015 Osman KOCAK <kocakosm@gmail.com>                   *
+ *                                                                            *
+ * This program is free software: you can redistribute it and/or modify it    *
+ * under the terms of the GNU Lesser General Public License as published by   *
+ * the Free Software Foundation, either version 3 of the License, or (at your *
+ * option) any later version.                                                 *
+ * This program is distributed in the hope that it will be useful, but        *
+ * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY *
+ * or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public     *
+ * License for more details.                                                  *
+ * You should have received a copy of the GNU Lesser General Public License   *
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.       *
+ *----------------------------------------------------------------------------*/
+
+package org.kocakosm.pitaya.net;
+
+import static org.kocakosm.pitaya.charset.Charsets.*;
+import static org.kocakosm.pitaya.net.MediaType.*;
+import static org.junit.Assert.*;
+
+import java.nio.charset.IllegalCharsetNameException;
+import java.util.HashMap;
+import java.util.Map;
+
+import org.junit.Test;
+
+/**
+ * {@link MediaType}'s unit tests.
+ *
+ * @author Osman KOCAK
+ */
+public final class MediaTypeTest
+{
+	private static final Map<String, String> PARAMS = new HashMap<String, String>() {{
+		put("a", "1");
+		put("b", "2");
+		put("c", "3");
+	}};
+
+	@Test
+	public void testParse()
+	{
+		MediaType m1 = parse("image/png");
+		assertEquals("image", m1.type());
+		assertEquals("png", m1.subtype());
+		assertTrue(m1.parameters().isEmpty());
+
+		MediaType m2 = parse("Application/atom+XML; a=1; b=2; c=3");
+		assertEquals("application", m2.type());
+		assertEquals("atom+xml", m2.subtype());
+		assertEquals(PARAMS, m2.parameters());
+
+		MediaType m3 = parse("application/vnd.net-fpx;a.B-c=\"?\\\\?D\"");
+		assertEquals("application", m3.type());
+		assertEquals("vnd.net-fpx", m3.subtype());
+		assertEquals(m3.parameters().get("a.b-c"), "?\\?D");
+	}
+
+	@Test(expected = IllegalArgumentException.class)
+	public void testParseEmptyString()
+	{
+		parse("");
+	}
+
+	@Test
+	public void testParseInvalidString()
+	{
+		testParseFailure("/");
+		testParseFailure("text");
+		testParseFailure("text/");
+		testParseFailure("te<t/plain");
+		testParseFailure("text/pl@in");
+		testParseFailure("text/plain;");
+		testParseFailure("text/plain; ");
+		testParseFailure("text/plain; a");
+		testParseFailure("text/plain; a=");
+		testParseFailure("text/plain; a=@");
+		testParseFailure("text/plain; a=\"@");
+		testParseFailure("text/plain; a=1;");
+		testParseFailure("text/plain; a=1; ");
+		testParseFailure("text/plain; a=1; b");
+		testParseFailure("text/plain; a=1; b=");
+		testParseFailure("text/plain; a=\u2025");
+	}
+
+	private void testParseFailure(String input)
+	{
+		IllegalArgumentException expected = null;
+		try {
+			parse(input);
+		} catch (IllegalArgumentException e) {
+			expected = e;
+		}
+		assertNotNull(expected);
+	}
+
+	@Test(expected = IllegalArgumentException.class)
+	public void testCreateWithInvalidType()
+	{
+		create("?", "plain");
+	}
+
+	@Test(expected = IllegalArgumentException.class)
+	public void testCreateWithInvalidSubtype()
+	{
+		create("text", "pl@in");
+	}
+
+	@Test(expected = IllegalArgumentException.class)
+	public void testCreateWithWildcardTypeDeclaredSubtype()
+	{
+		create("*", "text");
+	}
+
+	@Test
+	public void testType()
+	{
+		assertEquals("text", PLAIN_TEXT.type());
+	}
+
+	@Test
+	public void testSubtype()
+	{
+		assertEquals("xml", XML.subtype());
+	}
+
+	@Test
+	public void testParameters()
+	{
+		assertTrue(PNG.parameters().isEmpty());
+		assertEquals(PARAMS, PNG.withParameters(PARAMS).parameters());
+	}
+
+	@Test
+	public void testWithoutParameters()
+	{
+		assertEquals(OCTET_STREAM, OCTET_STREAM.withoutParameters());
+		assertEquals(GIF, parse("image/gif; foo=bar").withoutParameters());
+	}
+
+	@Test
+	public void testWithParameters()
+	{
+		assertEquals(
+			parse("image/jpeg; a=1; b=2; c=3"),
+			JPEG.withParameters(PARAMS));
+		assertEquals(
+			parse("text/plain; a=1; b=2; c=3"),
+			parse("text/plain; a=0; b=2").withParameters(PARAMS));
+		assertEquals(
+			parse("text/plain; a=1; b=2; c=3"),
+			parse("text/plain; a=1; b=2; c=3").withParameters(PARAMS));
+	}
+
+	@Test(expected = IllegalArgumentException.class)
+	public void testWithParametersWithInvalidAttribute()
+	{
+		HTML.withParameters(new HashMap<String, String>() {{
+			put("a", "1");
+			put("@", "2");
+			put("c", "3");
+		}});
+	}
+
+	@Test
+	public void testWithoutParameter()
+	{
+		assertEquals(CSS, CSS.withoutParameter("a"));
+		assertEquals(
+			parse("text/plain; a=1"),
+			parse("text/plain; a=1; b=2").withoutParameter("b"));
+	}
+
+	@Test
+	public void testWithParameter()
+	{
+		assertEquals(
+			parse("text/plain; a=1"),
+			parse("text/plain").withParameter("a", "1"));
+		assertEquals(
+			parse("text/plain; a=1"),
+			parse("text/plain; a=1; a=2").withParameter("a", "1"));
+		assertEquals(
+			parse("text/plain; a=3"),
+			parse("text/plain; a=1; a=2").withParameter("a", "3"));
+		assertEquals(
+			parse("text/plain; a=1; b=2; c=3"),
+			parse("text/plain; a=1; b=2").withParameter("c", "3"));
+	}
+
+	@Test(expected = IllegalArgumentException.class)
+	public void testWithParameterWithInvalidAttribute()
+	{
+		JSON.withParameter("@", "2");
+	}
+
+	@Test
+	public void testWithCharset()
+	{
+		assertEquals(
+			parse("text/plain; charset=utf-8"),
+			parse("text/plain").withCharset(UTF_8));
+		assertEquals(
+			parse("text/plain; charset=utf-8"),
+			parse("text/plain; charset=utf-16").withCharset(UTF_8));
+	}
+
+	@Test
+	public void testCharset()
+	{
+		MediaType m1 = PLAIN_TEXT;
+		assertNull(m1.charset());
+		m1 = m1.withParameter("charset", "utf-8");
+		assertEquals(UTF_8, m1.charset());
+		assertEquals("UTF-8", m1.parameters().get("charset"));
+
+		MediaType m2 = parse("text/plain; CHARSET=utf-8");
+		assertEquals(UTF_8, m2.charset());
+		assertEquals("UTF-8", m2.parameters().get("charset"));
+	}
+
+	@Test(expected = IllegalCharsetNameException.class)
+	public void testIllegalCharset()
+	{
+		parse("text/plain; charset=\"???\"").charset();
+	}
+
+	@Test
+	public void testHasWildcard()
+	{
+		assertFalse(PLAIN_TEXT.hasWildcard());
+		assertFalse(JPEG.hasWildcard());
+		assertTrue(ANY_TYPE.hasWildcard());
+		assertTrue(ANY_APPLICATION_TYPE.hasWildcard());
+		assertTrue(ANY_AUDIO_TYPE.hasWildcard());
+		assertTrue(ANY_IMAGE_TYPE.hasWildcard());
+		assertTrue(ANY_TEXT_TYPE.hasWildcard());
+		assertTrue(ANY_VIDEO_TYPE.hasWildcard());
+	}
+
+	@Test
+	public void testIs()
+	{
+		assertTrue(PLAIN_TEXT.is(ANY_TYPE));
+		assertTrue(JPEG.is(ANY_TYPE));
+		assertTrue(ANY_TEXT_TYPE.is(ANY_TYPE));
+		assertTrue(PLAIN_TEXT.is(ANY_TEXT_TYPE));
+		assertTrue(PLAIN_TEXT.is(ANY_TEXT_TYPE));
+		assertFalse(JPEG.is(ANY_TEXT_TYPE));
+		assertTrue(PLAIN_TEXT.is(PLAIN_TEXT));
+		assertFalse(PLAIN_TEXT.is(HTML));
+		assertFalse(PLAIN_TEXT.is(PLAIN_TEXT.withCharset(UTF_8)));
+		assertFalse(PLAIN_TEXT.withCharset(UTF_16).is(PLAIN_TEXT.withCharset(UTF_8)));
+	}
+
+	@Test
+	public void testEqualsAndHashCode()
+	{
+		MediaType m0 = null;
+		MediaType m1 = create("A", "B").withParameter("C", "D");
+		MediaType m2 = parse("a/B;c=D");
+		MediaType m3 = parse("A/b;  C=D");
+		MediaType m4 = parse("a/b;c=e");
+		MediaType m5 = parse("a/z;c=e");
+
+		assertFalse(m1.equals(m0));
+		assertEquals(m1, m1);
+		assertEquals(m1.hashCode(), m1.hashCode());
+		assertEquals(m1, m2);
+		assertEquals(m2, m1);
+		assertEquals(m2, m3);
+		assertEquals(m1, m3);
+		assertEquals(m1.hashCode(), m2.hashCode());
+		assertEquals(m2.hashCode(), m3.hashCode());
+		assertNotEquals(m1, m4);
+		assertNotEquals(m4, m1);
+		assertNotEquals(m2, m4);
+		assertNotEquals(m4, m5);
+		assertNotEquals(m2, m5);
+		assertNotEquals(m3, m4);
+		assertNotEquals(m4, m5);
+	}
+
+	@Test
+	public void testToString()
+	{
+		assertEquals("text/plain", create("text", "plain").toString());
+		MediaType test = create("text", "plain").withParameter("a", "!@#$%\" ^&*()");
+		assertEquals("text/plain; a=\"!@#$%\\\" ^&*()\"", test.toString());
+	}
+}
