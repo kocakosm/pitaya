8a6daf586d5a5bd84cdb1b6abfbf5ee16180f847
Osman Koçak <kocakosm@gmail.com>
2016-08-21 22:02:34 +0200

    Updated copyright headers

--- src/org/kocakosm/pitaya/security/PBKDF1.java
+++ src/org/kocakosm/pitaya/security/PBKDF1.java
@@ -1,6 +1,6 @@
 /*----------------------------------------------------------------------------*
  * This file is part of Pitaya.                                               *
- * Copyright (C) 2012-2015 Osman KOCAK <kocakosm@gmail.com>                   *
+ * Copyright (C) 2012-2016 Osman KOCAK <kocakosm@gmail.com>                   *
  *                                                                            *
  * This program is free software: you can redistribute it and/or modify it    *
  * under the terms of the GNU Lesser General Public License as published by   *
